96c521e0837db3ca1d107ed56cbe76a7dfb08e54
Jannis R <mail@jannisr.de>
2020-05-25 03:04:55 +0200

    simplify

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const {ok, strictEqual} = require('assert')
-const parallelTransform = require('./lib/parallel-transform')
+const {transform: parallelTransform} = require('parallel-stream')
 
 const basicLatency = (ms) => {
 	ok(Number.isInteger(ms), 'ms must be an integer')
@@ -42,20 +42,27 @@ const basicLoss = (rate) => {
 
 const _lossBy = (frame, getT) => {
 	strictEqual(typeof frame, 'function', 'frame must be a function')
-	let i = 0
-	let f = frame(getT(), i++)
-	ok(Number.isInteger(f.from), 'frame(i).from must be a finite number')
-	ok(Number.isInteger(f.length), 'frame(i).length must be a finit number')
 
+	let f = {from: getT(), length: 1}, i = 0
+	// console.error('first frame', f)
 	const withLossByTime = (chunk, push, done) => {
 		const t = getT()
 		// frame over? -> generate new
-		if (f && t >= f.from + f.length) f = frame(t, i++)
+		if (!f || (f && t >= f.from + f.length)) {
+			f = frame(t, i++)
+			// console.error('new frame', f, t, chunk)
+		}
 
 		// within frame? -> drop chunk
-		if (f && t >= f.from && t < f.from + f.length) done()
+		if (f && t >= f.from && t < f.from + f.length) {
+			// console.error('drop', t, chunk)
+			done()
+		}
 		// outside of frame? -> pass chunk on
-		else done(null, chunk)
+		else {
+			// console.error('pass', t, chunk)
+			done(null, chunk)
+		}
 	}
 	return withLossByTime
 }
@@ -69,15 +76,34 @@ const lossByPacketIdx = (frame) => {
 	return _lossBy(frame, () => idx++)
 }
 
-const simulateNetworkConditions = (transforms, opt = {}) => {
-	ok(Array.isArray(transforms), 'transforms must be an array')
-	const lTransforms = transforms.length
-	ok(lTransforms > 0, 'transforms must not be empty')
+const simulateNetworkConditions = (transform, opt = {}) => {
+	strictEqual(typeof transform, 'function', 'transform must be a function')
 
-	return parallelTransform(transforms, {
+	const work = (chunk, _, done) => {
+		try {
+			transform(chunk, push, (err, chunk) => {
+				if (err) {
+					done(err)
+					return;
+				}
+				if (chunk !== null && chunk !== undefined) {
+					out.push(chunk)
+				}
+				done()
+			})
+		} catch (err) {
+			done(err)
+		}
+	}
+
+	const out = parallelTransform(work, {
 		concurrency: 50, // todo: this is hacky
 		...opt,
+		objectMode: true,
 	})
+	const push = out.push.bind(out)
+
+	return out
 }
 
 simulateNetworkConditions.basicLatency = basicLatency
